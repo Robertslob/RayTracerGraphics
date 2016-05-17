2de2462b1d6858f27c36f7e2a1384c78beaff3f5
Zino <zinoonomiwo@gmail.com>
2016-05-17 10:08:06 +0200

    Revert "Fuck Robert :+1:"
    This reverts commit d21e4becbc9c4c18ad49a3c3158e1210255e2d15.

---
+++ vector.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace template
+{
+    public class Vector
+    {
+        public float x, y, z;
+
+        public Vector(float x, float y, float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        // TODO: add functions as inproduct, crossproduct ?
+    }
+}
