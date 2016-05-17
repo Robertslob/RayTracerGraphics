eddee9e43281b56af1b6e5226b117527fcb038f9
Zino <zinoonomiwo@gmail.com>
2016-05-17 10:32:47 +0200

    start camera

--- camera.cs
+++ camera.cs
@@ -3,16 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenTK.Graphics;
+using OpenTK;
 
 namespace template
 {
     public class Camera
     {
-        Vector cameraPosition;
-        float upLeft, upRight, downLeft, downRight;
+        Vector3 cameraPosition;
+        Vector3 position;
+        Vector3 direction;
         // vector Direction?
+        Vector3 p1, p2, p3;
 
-        public Camera(Vector v)
+        public Camera(Vector3 v)
         {
             cameraPosition = v;
             UpdatePlane();
@@ -21,6 +25,16 @@ namespace template
         public void UpdatePlane()
         {
             // hardcode the screen corners
+            Vector3 up = Vector3.UnitZ;
+            Vector3 left = Vector3.Cross(direction, up);
+            up = Vector3.Cross(left, direction);
+
+            int dist = 123;
+            p1 = direction * dist + left - up;
+            p2 = direction * dist + left + up;
+            p3 = direction * dist - left + up;
+
         }
+
     }
 }
