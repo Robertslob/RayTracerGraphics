0b2626a1db0eb1028df058d064313aee1fafb128
Robertslob <rjrslob@gmail.com>
2016-05-18 17:07:19 +0200

    Have fun met shadowrays

--- Scene.cs
+++ Scene.cs
@@ -15,7 +15,7 @@ namespace Application
         public Scene()
         {
             //all the primitives that are present in our scene
-            allLights.Add(new Light(new Vector3(10, -2, 10), new Vector3(10, 10, 10)));
+            allLights.Add(new Light(new Vector3(0, 0, 0), new Vector3(10, 10, 10)));
             allPrimitives.Add(new Plane(new Vector3(0, 1, 0), new Vector3(0, 0, 0), new Material(new Vector3(0.2f, 0.2f, 0.3f), 0.0f, 0.4f)));
             allPrimitives.Add(new Sphere(new Vector3(0, 1, 0), 1, new Material(new Vector3(0, 1, 0), 0.5f, 0.98f)));
             allPrimitives.Add(new Sphere(new Vector3(-3, 1, 0), 1, new Material(new Vector3(1, 0, 0), 0.5f, 0.0f)));
@@ -38,8 +38,39 @@ namespace Application
                     closestPrimitive = primitive;
                 }
             }
-            //returns the closest primitive if there is an intersection, else it returns a self made intersection...
-            return new Intersection(closestDistance, closestPrimitive);
+            //returns the closest primitive if there is an intersection, else it returns null
+            if (closestDistance == 1000000)
+                return new Intersection(closestDistance, null);
+            else
+            {
+                if(!shadowRay(ray.Origin + ray.Direction * closestDistance))
+                    return new Intersection(closestDistance, closestPrimitive);
+            }
+            return new Intersection(closestDistance, null);
+        }
+
+        // Return true als onzichtbaar, false anders
+        public bool shadowRay (Vector3 point)
+        {
+            foreach(Light light in allLights)
+            {
+                Vector3 loc = light.location;
+                Vector3 dir = point - loc;
+                float length = (float)Math.Sqrt((Vector3.Dot(dir, dir)));
+                Vector3 normDir = dir / length;
+                Ray r = new Ray(loc, normDir);
+                float closestDistance = length*.99f;
+                float currentDistance;
+
+                foreach (Primitive primitive in allPrimitives)
+                {                    
+                    currentDistance = primitive.intersects(r);
+                    //we loop over all primitives and return the intersect of the closest one which we intersect
+                    if (closestDistance > currentDistance && currentDistance > 0)
+                        return true;
+                }
+            }
+            return false;
         }
 
 
